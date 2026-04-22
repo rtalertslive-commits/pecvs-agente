319eea5ec5108ef0a72934727e5a5621bf9ad6a9
Franco <franco@placeholder.com>
2026-04-22 14:21:45 -0600

    Added automatic SW update detection and skipWaiting logic (v1.0.5)

--- sw.js
+++ sw.js
@@ -2,8 +2,17 @@
 const CACHE_NAME = 'pecvs-agent-v1';
 const assets = ['./', './index.html'];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+    self.skipWaiting(); // FUERZA LA ACTIVACIÓN DE LA NUEVA VERSIÓN
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim()); // TOMA EL CONTROL DE LA PÁGINA INMEDIATAMENTE
 });
 
 self.addEventListener('fetch', e => {
